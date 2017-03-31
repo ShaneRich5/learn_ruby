0b73c7b07d23256f72c1b630da6fb31a8835f2d6
Shane Richards <shane.richards121@gmail.com>
2017-03-31 05:14:28 +0000

    completed titleize

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
 #write your code here
+def echo(str) 
+   str
+end
+
+def shout(str)
+    str.upcase 
+end
+
+def repeat(str, n=2)
+    ([str] * n).join(' ')
+end
+
+def start_of_word(word, n)
+    word[0..n - 1]
+end
+
+def first_word(sentence)
+    sentence.split.first
+end
+
+def titleize(string)
+	split_array=string.split(" ")
+	split_array.map! do |word|
+		if word == "and" || word== "the" || word=="over"
+			if split_array.index(word)==0
+				word.capitalize
+			else word
+			end
+		else 
+			word.capitalize
+		end
+	end
+	split_array
+	return split_array.join(" ")
+end
