eace68aa3cc46e2bfada499a2030cb61b31d8ac6
Shane Richards <shane.richards121@gmail.com>
2017-04-03 23:04:53 +0000

    complited exercise 4

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,24 @@
 #write your code here
+def translate(sentence)
+    words = sentence.split(' ')
+    words.map! { |word| rearrange(word) + 'ay' }
+    words.join(' ')
+end
+
+def rearrange(word)
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    
+    word.size.times do
+        first_letter = word[0]
+        if 'qu' == word.slice(0, 2)
+            letters = word.slice!(0, 2)
+            word << letters
+        elsif not vowels.include? first_letter
+            word << first_letter
+            word.slice!(0)
+        else
+            break 
+        end
+    end
+    return word
+end
