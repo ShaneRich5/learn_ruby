c734ccfbe5a14cfe0d5027e52f75d3600b1449e9
Shane Richards <shane.richards121@gmail.com>
2017-04-04 01:53:47 +0000

    complete book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,22 @@
 class Book
-# write your code here
+    attr_accessor :title
+    
+    def title
+        titleize(@title.capitalize!)        
+    end
+    
+    private
+    def titleize(words)
+        little_words = ["And","The","A","An","In","Of",""]
+		words_arry = words.split(" ")
+		for i in 1...words_arry.length
+			words_arry[i].capitalize!
+			if little_words.include? words_arry[i]
+			    words_arry[i].downcase! 
+			end
+			
+		end
+
+		words_arry.join(" ")
+    end
 end
